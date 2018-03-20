f0704e7067656cb7057b15bceef8708a7009c355
abdelilah bouyebra <abdelilah.bouyebra@gmail.com>
2018-03-20 15:26:10 +0000

    Login (#7)
    * precommit
    
    * teeeest
    
    * refaire
    
    * add component users
    
    * add componentLogin
    
    * teeeest
    
    * refaire
    
    * add component users
    
    * add componentLogin
    
    * add component listUsers

--- projectGit/src/app/app.module.ts
+++ projectGit/src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './users/login/login.component';
+import { ListUsersComponent } from './users/list-users/list-users.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    ListUsersComponent,
     LoginComponent
   ],
   imports: [
